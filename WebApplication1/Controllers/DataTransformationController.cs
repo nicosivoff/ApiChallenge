20651aa9cebc0224e9471252e185efa551f7c44c
Nicolás Sivoff <nicosivoff@gmail.com>
2022-08-19 23:09:14 -0300

    Agregar archivos de proyecto.

---
+++ WebApplication1/Controllers/DataTransformationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataTransformationController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+        }
+    }
+}
